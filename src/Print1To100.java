bc9e93fc2de8117909e5ddd40fbd09b939be3df3
poornima2089 <poornimabpatil89@gmail.com>
2020-03-10 11:33:18 -0400

    This is my 1st commit to print 1-100

---
+++ src/Print1To100.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Print1To100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+     System.out.println("The 1 to 100 numbers are:");
+     for(int i=1;i<=100;i++) {
+    	 System.out.println(""+i);
+     }
+     
+	}
+}
